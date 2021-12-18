3586e338c0722c3ecd6708f9730182410a5ce0c2
Angelo Bongiorno <angelobongiorno.ab@gmail.com>
2021-12-18 19:16:15 +0100

    shapes implementano ora eventuale lista di stereotypes

--- parser/parser.py
+++ parser/parser.py
@@ -5,16 +5,36 @@ from diagram import Diagram
 from connectors import Connector
 from shape import Shape
 
-tree = ET.parse('./assets/project.xml')
+#class Parser:
+#   def __init__(self, path: str):
+#       tree = ET.parse(path)
+#       root = tree.getroot()
+
+tree = ET.parse('./assets/class_diagram_3.xml')
 root = tree.getroot()
 
 diagram = Diagram(root[2][0].attrib['Width'], root[2][0].attrib['Height'])
 
-# shapes
-for element in root[2][0][0]:
-    diagram.shapes.append(Shape(element.attrib['Name'], element.attrib['X'], element.attrib['Y'], element.attrib['Background'], element.attrib['Width'], element.attrib['Height']))
 
-# connectors    
+# shapes:
+
+for index, element in enumerate(root[2][0][0]):
+    model = element.attrib['Model']
+    
+    diagram.shapes.append(Shape(element.attrib['Name'], element.attrib['Model'], element.attrib['X'], element.attrib['Y'], element.attrib['Background'], element.attrib['Width'], element.attrib['Height']))
+
+    # nel tag model (root[1]), scorre tutti gli elementi finché non trova quello
+    # corrispondente alla shape appena istanziata.
+    for class_instance in root[1]:
+        if class_instance.attrib['Id'] == model:
+            # Trovata, scorre tutte le sottoclassi dell'elemento fino a trovare la sottoclasse 'Stereotypes'
+            for sub_class in class_instance:
+                if sub_class.tag == 'Stereotypes':
+                    diagram.shapes[index].matchStereotypes(sub_class)
+
+
+# connectors:
+#   
 for element in root[2][0][1]:
     print(element.tag)
     for index, point in enumerate(element):
--- parser/shape.py
+++ parser/shape.py
@@ -1,9 +1,21 @@
+from typing import List
+
+
 class Shape:
 
-    def __init__(self, name: str, x: int, y: int, bgcolor: str, width: int, height: int) -> None:
+    def __init__(self, name: str, model: str, x: int, y: int, bgcolor: str, width: int, height: int) -> None:
         self.name = name
+        self.model = model
         self.x = x
         self.y = y
         self.bgcolor = bgcolor
         self.width = width
         self.height = height
+        self.stereotypes = []
+
+    # serve ad aggiungere alla shape eventuale elenco di stereotypes che implementa, questi sono necessari
+    # perché mostrati nella rappresentazione grafica del diagramma
+    def matchStereotypes(self, stypes):
+        for stereotype in stypes:
+            self.stereotypes.append(stereotype.attrib['Name'])
+            # print('questa è una ' + stereotype.attrib['Name'])
